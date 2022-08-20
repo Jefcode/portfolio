c0def5bb7008040dc9d21c1fc02c1246980137cf
Jefcode <am.amir.husein@gmail.com>
2022-08-20 20:21:16 +0430

    finish project

---
+++ main.js
@@ -0,0 +1,7 @@
+import './style.css';
+import Experience from './src/Experience/Experience.js';
+
+// WebGL Experience
+const experience = new Experience(
+	document.querySelector('canvas.experience-convas')
+);
